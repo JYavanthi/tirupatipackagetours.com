6f6540a5d8136de1b895d02565e266ed1e1af0a0
Akash Dey <akashdeypersonal17@gmail.com>
2025-09-30 18:32:08 +0530

    abcd

--- node_modules/backend/server.js
+++ node_modules/backend/server.js
@@ -175,7 +175,7 @@ require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname)));
 
 // ------------------- DATABASE CONFIG -------------------
 const dbConfig = {
@@ -196,8 +196,8 @@ const dbConfig = {
 };
 
 // React routes
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 
